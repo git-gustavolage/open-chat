2a3b8cb74ee4e1dbec7bca0178bbb8d364d06421
Gustavo Lage <lageg901@gmail.com>
2025-09-22 06:51:59 -0400

    update port

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3000,
+    port: 9000,
     strictPort: true,
     host: true
   }
